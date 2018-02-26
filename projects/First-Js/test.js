a41ddc4648d874f5167d751c7b3e943d7f3c574b
Mohamad Alsayed <mohamad.alsayed93@gmail.com>
2018-02-26 16:42:20 +0100

    my first java script

---
+++ projects/First-Js/test.js
@@ -0,0 +1,29 @@
+console.log('Hello World! from external script');
+
+console.info('Hi');
+
+
+console.log('Hello World from internal script index html!');
+console.info('Hi');
+var price1 = 6;
+var price2 = 5;
+var total = price1 + price2;
+var txt = "1";
+var demos = price1 + txt;
+
+const strname = "Mohammad";
+
+console.info("My name is:" + " " + strname);
+
+const float = 3.2;
+console.info( float);
+console.info("ceil is:" + Math.ceil(float));
+console.info(Math.ceil(5.9));
+console.info(Math.round(5.4));
+
+
+console.info(Boolean(price1 < price2));
+
+console.info("My name is:" + " " + strname);
+
+
